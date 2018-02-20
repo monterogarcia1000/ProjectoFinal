7d1d22488756cc94a10ca8b87a266328d1e9a8be
monterogarcia1000 <monterogarcia1000@gmail.com>
2018-02-20 20:09:07 +0100

    12345678

---
+++ front-spring-rest/src/main/java/com/rest/spring/front/SecureApplication.java
@@ -0,0 +1,19 @@
+package com.rest.spring.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * com.learningjava.rest.spring.front
+ * Class
+ * By berto. 12/02/2018
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = "com.rest.spring.front")
+public class SecureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureApplication.class, args);
+    }
+}
